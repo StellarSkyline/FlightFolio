9d7f836bf91f78ef49f86fd1b5e60f9dcb4dd67c
Seth Osric Torralba <sethosrictorralba@gmail.com>
2024-11-26 16:49:33 -0600

    Updates

--- app/src/main/java/com/example/flightfolio/ui/screen/LoginScreen.kt
+++ app/src/main/java/com/example/flightfolio/ui/screen/LoginScreen.kt
@@ -1,22 +1,46 @@
 package com.example.flightfolio.ui.screen
 
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import com.example.flightfolio.ui.theme.FlightFolioTheme
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.flightfolio.ui.theme.FFColorList
 
 
 @Composable
 fun LoginScreen(onClick: (String) -> Any) {
 
-
-    Box(
+    Column(
         modifier = Modifier
             .fillMaxSize()
-            .background(Color(0xFFD3D3D3))
-    )
+            .background(FFColorList.bg_color),
+        verticalArrangement = Arrangement.Top,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
 
+        Spacer(modifier = Modifier.height(30.dp))
+
+        Text(
+            text = "FlightFolio",
+            fontSize = 48.sp,
+            color = FFColorList.txt_primary
+        )
+
+    }
+
+}
+
+@Preview
+@Composable
+fun LoginScreenPreview() {
+    LoginScreen { }
 }
---
+++ app/src/main/java/com/example/flightfolio/ui/theme/FFColorList.kt
@@ -0,0 +1,8 @@
+package com.example.flightfolio.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+object FFColorList {
+    val bg_color = Color(0xFFD3D3D3)
+    val txt_primary = Color(0xFF263C52)
+}
