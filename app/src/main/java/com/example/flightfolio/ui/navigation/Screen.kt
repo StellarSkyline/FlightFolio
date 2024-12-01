601c45c664d5c057c927ef4f970fbac23f239ab2
Seth Osric Torralba <sethosrictorralba@gmail.com>
2024-12-01 13:28:28 -0600

    Login Update

--- app/src/main/java/com/example/flightfolio/ui/navigation/Screen.kt
+++ app/src/main/java/com/example/flightfolio/ui/navigation/Screen.kt
@@ -3,6 +3,7 @@ package com.example.flightfolio.ui.navigation
 sealed class Screen(val route:String) {
     object LoginScreen : Screen("login_screen")
     object LoginGraph:Screen("login_graph")
+    object RegisterScreen:Screen("register_screen")
 
 
 }
---
+++ app/src/main/java/com/example/flightfolio/ui/screen/RegisterScreen.kt
@@ -0,0 +1,12 @@
+package com.example.flightfolio.ui.screen
+
+import androidx.compose.runtime.Composable
+import com.example.flightfolio.viewmodel.LoginViewModel
+
+@Composable
+fun RegisterScreen(vm: LoginViewModel, onNavigate: (String) -> Any) {
+
+
+    
+
+}
