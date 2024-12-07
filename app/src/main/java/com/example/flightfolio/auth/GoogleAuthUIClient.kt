977875b07356177cd9f7149301419cd1dbe6fbe6
Seth Osric Torralba <sethosrictorralba@gmail.com>
2024-12-06 22:25:20 -0600

    Updated GoogleAuthUIClient
    Added to DI

--- app/src/main/java/com/example/flightfolio/auth/GoogleAuthUIClient.kt
+++ app/src/main/java/com/example/flightfolio/auth/GoogleAuthUIClient.kt
@@ -1,99 +1,11 @@
 package com.example.flightfolio.auth
 
-import android.app.Application
 import android.content.Intent
 import android.content.IntentSender
-import com.example.flightfolio.R
-import com.google.android.gms.auth.api.identity.BeginSignInRequest
-import com.google.android.gms.auth.api.identity.SignInClient
-import com.google.firebase.Firebase
-import com.google.firebase.auth.GoogleAuthProvider
-import com.google.firebase.auth.auth
-import kotlinx.coroutines.CancellationException
-import kotlinx.coroutines.tasks.await
 
-class GoogleAuthUIClient(
-    val app: Application,
-    val oneTapClient: SignInClient,
-) {
-
-    private val auth = Firebase.auth
-
-    suspend fun signIn(): IntentSender? {
-        val result = try {
-            oneTapClient.beginSignIn(
-                beginSignInRequest()
-            ).await()
-
-        } catch (e: Exception) {
-            e.printStackTrace()
-            if (e is CancellationException) throw e
-            null
-        }
-
-        return result?.pendingIntent?.intentSender
-
-    }
-
-    suspend fun signInWithIntent(intent: Intent): SignedInResult {
-        val credential = oneTapClient.getSignInCredentialFromIntent(intent)
-        val googleIdToken = credential.googleIdToken
-        val googleCredentials = GoogleAuthProvider.getCredential(googleIdToken, null)
-
-        return try {
-
-            val user = auth.signInWithCredential(googleCredentials).await().user
-            SignedInResult(
-                data = user?.run {
-                    UserData(
-                        userId = uid,
-                        userName = displayName!!,
-                        profilePictureUrl = photoUrl?.toString(),
-                    )
-                },
-                errorMessage = null
-            )
-
-        } catch (e: Exception) {
-            e.printStackTrace()
-            if (e is CancellationException) throw e
-            SignedInResult(
-                data = null,
-                errorMessage = e.message
-            )
-        }
-    }
-
-    suspend fun signOut() {
-        try {
-            oneTapClient.signOut().await()
-            auth.signOut()
-
-        } catch (e: Exception) {
-            e.printStackTrace()
-            if (e is CancellationException) throw e
-        }
-    }
-
-    fun getSignedInUser(): UserData? = auth.currentUser?.run {
-        UserData(
-            userId = uid,
-            userName = displayName!!,
-            profilePictureUrl = photoUrl?.toString()
-        )
-    }
-
-    private fun beginSignInRequest(): BeginSignInRequest {
-        return BeginSignInRequest.Builder()
-            .setGoogleIdTokenRequestOptions(
-                BeginSignInRequest.GoogleIdTokenRequestOptions.builder()
-                    .setSupported(true)
-                    .setFilterByAuthorizedAccounts(false)
-                    .setServerClientId(app.getString(R.string.web_client_id))
-                    .build()
-            )
-            .setAutoSelectEnabled(true)
-            .build()
-    }
-
+interface GoogleAuthUIClient {
+    suspend fun signIn(): IntentSender?
+    suspend fun signInWithIntent(intent: Intent): SignedInResult
+    suspend fun signOut()
+    fun getSignedInUser(): UserData?
 }
--- app/src/main/java/com/example/flightfolio/di/AppModule.kt
+++ app/src/main/java/com/example/flightfolio/di/AppModule.kt
@@ -1,11 +1,16 @@
 package com.example.flightfolio.di
 
 import android.content.Context
-import com.example.flightfolio.data.interfaces.UserPreferences
-import com.example.flightfolio.data.repo.UserPreferencesImpl
-import com.example.flightfolio.data.repo.LoginRegisterRepoImpl
+import com.example.flightfolio.R
+import com.example.flightfolio.auth.GoogleAuthUIClient
+import com.example.flightfolio.auth.GoogleAuthUIClientImpl
 import com.example.flightfolio.data.interfaces.LoginRegisterRepo
-
+import com.example.flightfolio.data.interfaces.UserPreferences
+import com.example.flightfolio.data.repo.LoginRegisterRepoImpl
+import com.example.flightfolio.data.repo.UserPreferencesImpl
+import com.google.android.gms.auth.api.identity.BeginSignInRequest
+import com.google.android.gms.auth.api.identity.Identity
+import com.google.android.gms.auth.api.identity.SignInClient
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
@@ -19,8 +24,41 @@ object AppModule {
 
     @Provides
     @Singleton
-    fun providesDataStore(@ApplicationContext app: Context): UserPreferences = UserPreferencesImpl(app)
+    fun providesDataStore(@ApplicationContext app: Context): UserPreferences =
+        UserPreferencesImpl(app)
 
     @Provides
     @Singleton
-    fun providesLoginRegisterRepo(dataStore: UserPreferences): LoginRegisterRepo = LoginRegisterRepoImpl(dataStore)
+    fun providesLoginRegisterRepo(dataStore: UserPreferences): LoginRegisterRepo =
+        LoginRegisterRepoImpl(dataStore)
+
+    @Provides
+    @Singleton
+    fun providesBeginSignInRequest(@ApplicationContext app: Context): BeginSignInRequest {
+        return BeginSignInRequest.Builder()
+            .setGoogleIdTokenRequestOptions(
+                BeginSignInRequest.GoogleIdTokenRequestOptions.builder()
+                    .setSupported(true)
+                    .setFilterByAuthorizedAccounts(false)
+                    .setServerClientId(app.getString(R.string.web_client_id))
+                    .build()
+            )
+            .setAutoSelectEnabled(true)
+            .build()
+    }
+
+    @Provides
+    @Singleton
+    fun providesSignInClient(@ApplicationContext app: Context): SignInClient = Identity.getSignInClient(app)
+
+    @Provides
+    @Singleton
+    fun providesGoogleAuthUIClient(@ApplicationContext app:Context, oneTapClient: SignInClient, beginSignInRequest: BeginSignInRequest): GoogleAuthUIClient = GoogleAuthUIClientImpl(app, oneTapClient, beginSignInRequest)
+
+
+
+}
+
+
+
+
